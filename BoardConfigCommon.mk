7f5f0cb0c6aefc5016ef2baf7df9fff5dd053d90
TestaMic <mateidiac9@gmail.com>
2024-10-07 19:18:39 +0300

    Fixed derp ninja error

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -108,7 +108,7 @@ PRODUCT_ENFORCE_VINTF_MANIFEST_OVERRIDE := true
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(COMMON_PATH)/framework_compatibility_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml
+    vendor/derp/config/device_framework_matrix.xml
 
 # Vendor separation
 TARGET_COPY_OUT_VENDOR := system/vendor
